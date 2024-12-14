1b202a13b2cfdcb13c9a9b6222406ec75ccb1fe5
GaurabCodeLab <kumargaurab56@gmail.com>
2024-12-15 01:03:29 +0530

    google analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <GoogleAnalytics gaId="G-4ZN9EPJDT1" />
       </body>
     </html>
   );
