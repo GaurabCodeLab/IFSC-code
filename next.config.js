bba83bf95ae7a3626c6e8e30dda4028a9899f0a8
GaurabCodeLab <kumargaurab56@gmail.com>
2024-12-15 01:50:37 +0530

    some changes done

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "via.placeholder.com",
-      },
-      {
-        protocol: "https",
-        hostname: "yourwebsite.com",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-    ],
+    domains: ["via.placeholder.com", "yourwebsite.com", "res.cloudinary.com"],
   },
 };
 
