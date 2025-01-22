4c584544260e0ba7706ec4e691effb13c7d845bc
GaurabCodeLab <kumargaurab56@gmail.com>
2025-01-23 01:24:25 +0530

    meta data added

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,18 +3,20 @@ import BlogPost from "./BlogPost";
 import { getAllBlogPosts } from "./lib/api";
 
 export const metadata: Metadata = {
-  title: "Our Blog | Your Company Name",
+  title: "Our Blog | BankInfo Hub",
   description:
     "Read our latest articles and stay up to date with industry trends and insights.",
+  keywords:
+    "IFSC, bank details, ifsc code kya hota hai, branch lookup, Indian Financial System Code, airtel payment bank ifsc code, bank of baroda ifsc code, sbi ifsc code, indian bank ifsc code, ifsc code sbi, canara bank ifsc code, india post payment bank ifsc code",
   openGraph: {
-    title: "Our Blog | Your Company Name",
+    title: "Our Blog | BankInfo Hub",
     description:
       "Read our latest articles and stay up to date with industry trends and insights.",
     type: "website",
-    url: "https://yourwebsite.com/blog",
+    url: "https://ifsccodeb.com/blog",
     images: [
       {
-        url: "https://yourwebsite.com/images/blog-og-image.jpg",
+        url: "https://res.cloudinary.com/dqquvmo2p/image/upload/v1733082538/cover_u0ivck.webp",
         width: 1200,
         height: 630,
         alt: "Our Blog",
