40dfe0aa7a77c3db3240313e2026da41dd122321
GaurabCodeLab <kumargaurab56@gmail.com>
2025-03-08 21:10:25 +0530

    metadata changes

--- src/app/micr/[micr]/page.tsx
+++ src/app/micr/[micr]/page.tsx
@@ -2,11 +2,11 @@ import { Metadata } from "next";
 import MICRLookup from "./micr-lookup";
 
 export const metadata: Metadata = {
-  title: "IFSC Code Lookup | Find Bank Details",
+  title: "MICR code search | Find Bank Details",
   description:
-    "Enter your IFSC code to quickly find bank details including branch name, address, and more.",
+    "Enter your MICR code to quickly find bank details including branch name, address, and more.",
   keywords:
-    "IFSC, bank details, branch lookup, Indian Financial System Code, airtel payment bank ifsc code, bank of baroda ifsc code, sbi ifsc code, indian bank ifsc code, ifsc code sbi, canara bank ifsc code, india post payment bank ifsc code",
+    "micr code, micr code on cheque, micr code in cheque, what is micr code, micr code of bank, micr code means, micr code sbi, sbi micr code, union bank micr code, bank micr code, indian bank micr code, hdfc micr code, bank of baroda micr code",
 };
 
 export default function Home() {
